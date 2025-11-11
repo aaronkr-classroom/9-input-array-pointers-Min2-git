578db52cc17f2831ae4064c51692d75ebd7f7bf4
Min2-git <cactus9976@gmail.com>
2025-11-11 17:12:38 +0900

    week 10

---
+++ getMyStr.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#define MAX_CH 100
+
+int getMyStr(char buffer[], int limit) {
+    int i;
+
+    for (i = 0; i < limit - 1; i++) {  // 마지막에 '\0' 넣을 공간 확보
+        int ch = getchar();
+
+        if (ch == '\n' || ch == EOF) {  // 줄 끝이나 파일 끝
+            buffer[i] = '\0';
+            return 1;  // 정상 입력 종료
+        }
+
+        buffer[i] = (char)ch;
+    }
+
+    buffer[i] = '\0';  // 버퍼 끝에 NULL 문자 보장
+    return 0;           // 입력이 너무 길어서 '\n'을 못 만남
+}
+
+int main(void) {
+    char input_str[MAX_CH];
+    int state;
+
+    printf("Enter text (max %d chars): ", MAX_CH - 1);
+    state = getMyStr(input_str, MAX_CH);
+
+    if (state)
+        printf("input: %s\n", input_str);
+    else
+        printf("input: %s -> out of range\n", input_str);
+
+    return 0;
+}
---
+++ scanf.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    char char_data;
+    int int_data, oct_data, hex_data;
+    float float_data;
+    char str_data[20];
+
+    printf("Enter the following data:\n");
+    printf("%%c, %%d, %%o, %%x, %%f, %%s\n\n");
+
+    printf("Enter a character: ");
+    scanf_s(" %c", &char_data, 1);  
+    printf("char %%c: %c\n", char_data);
+
+    printf("Enter three integers (decimal, octal, hex): ");
+    scanf_s("%d %o %x", &int_data, &oct_data, &hex_data);
+    printf("int %%d: %d, oct %%o: %o, hex %%x: %x\n", int_data, oct_data, hex_data);
+
+    printf("Enter a floating number: ");
+    scanf_s("%f", &float_data);
+    printf("float %%f: %f\n", float_data);
+
+    printf("Enter a string: ");
+    scanf_s("%s", str_data, (unsigned)_countof(str_data));  
+    printf("str %%s: %s\n", str_data);
+
+    return 0;
+}
